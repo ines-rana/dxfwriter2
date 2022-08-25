fd6cd3a6db3d9895ebe76c258f472772e7e665dc
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-25 17:01:34 -0500

    .

--- src/api/parser.js
+++ src/api/parser.js
@@ -5,27 +5,40 @@
 */
 
 // query parameters
-//  format=json     (default is text/plain)
+//   format=json     (default is text/plain)
 
 
 const version = "2022-08"
 
 import parse, { DxfParser } from 'dxf-parser';
 
-
-
 export default function handler(req, res) {
-  res.setHeader('X-Version', version + '_31');
+  res.setHeader('X-Version', version + '_32');
 
-  var format = req.query.format;
-  if (!format) format = "";    // default
+  var format = req.query.format || ""; // default
 
   //console.log(req.files[0].buffer.toString());
   const parsed_input = parserFunction(req.files[0].buffer.toString());
 
-  format.toUpperCase() === "JSON" 
-    ? { if (parsed_input){ res.send(parsed_input).json(); } else { res.send({}).json(); } res.end(); }
-    : { if (parsed_input){ res.send(JSON.stringify(parsed_input, null, 2) + '\n'); } else { res.send(""); } res.end(); };
+  if (format.toUpperCase() === "JSON" )
+    {
+      if (parsed_input) {
+        res.send(parsed_input).json();
+      } else {
+        res.send({}).json();
+      }
+      res.end();
+    } 
+  else
+    {
+      if (parsed_input) {
+        res.send(JSON.stringify(parsed_input, null, 2) + '\n');
+      } else {
+        res.send("");
+      }
+      res.end();
+    };
+
   return;
 }
 
@@ -33,16 +46,18 @@ export default function handler(req, res) {
 
 
 function parserFunction(text) {
-//console.log("input length:", text.length);
+  //console.log("input length:", text.length);
 console.log("input length:", text.length);
   var parser = new DxfParser();
   try {
     var dxf = parser.parseSync(text);
 console.log("parser OK");
     return (dxf);
-  } catch(err) {
+  } catch (err) {
 console.error(err.stack);
     //console.error(err.stack);
-    return ({"error": err.stack});
+    return ({
+      "error": err.stack
+    });
   }
 }
