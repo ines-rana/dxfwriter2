f4f9405fcc247acfd291f7fe01ab26a7920996b5
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-25 14:39:28 -0500

    .

--- src/api/parser.js
+++ src/api/parser.js
@@ -22,30 +22,13 @@ const version = "2022-08"
 
 import parse, { DxfParser } from 'dxf-parser';
 
-module.exports = {
-  config :
-{
-   bodyParser: {
-     json: {
-       type: "application/json",
-       limit: "100kb"
-     },
-     raw: {
-       type: "application/octet-stream",
-       limit: "100kb"
-     },
-     text: {
-       type: "text/plain",
-       limit: "100kb"
-     },
-     urlencoded: {
-       type: "application/x-www-form-urlencoded",
-       limit: "100kb",
-       extended: false      // default is    true
-     }
-   }
- }
-};
+export const config = {
+  bodyParser: {
+    raw: {
+      type: `*/*`,
+    },
+  },
+}
 
 export default function handler(req, res) {
   res.setHeader('X-Version', version);
