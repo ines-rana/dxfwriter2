46f5e3cbc276b43c6a8de3d8e67e1490a369bd76
ines-rana <57879729+ines-rana@users.noreply.github.com>
2022-08-25 13:26:22 -0500

    .

--- src/api/parser.js
+++ src/api/parser.js
@@ -1,6 +1,22 @@
+/*
+We parse commonly used data types. You can parse more by adding custom
+middleware. Data available by default on the req object:
+
+   Cookies at req.cookies
+   URL Queries (e.g. api/foo?query=foo) at req.query
+   Form parameters and data at req.body
+   JSON POST bodies at req.body
+   Files uploaded from forms at req.files
+      E.g. curl -F xls=@file.xls ... will set
+      req.files[0] =  {"fieldname":"xls","originalname":"file.xls","encoding":"7bit","mimetype":"application/octet-stream","buffer":{"type":"Buffer","data":[123,...,123]},"size":524}
+  req object keys: ["_readableState","_events","_eventsCount","_maxListeners","socket","httpVersionMajor","httpVersionMinor","httpVersion","complete","headers","rawHeaders","trailers","rawTrailers","aborted","upgrade","url","method","statusCode","statusMessage","client","_consuming","_dumped","next","baseUrl","originalUrl","_parsedUrl","params","query","res","body","cookies"]
+*/
+
+const version = "2022-08"
 const Drawing = require('dxf-writer');
 const fs = require('fs');
 
+export default function handler(req, res) {
 let d = new Drawing();
 
 d.setUnits('Decimeters');
@@ -14,10 +30,13 @@ d.addLayer('l_yellow', Drawing.ACI.YELLOW, 'DOTTED')
  .setActiveLayer('l_yellow')
  .drawCircle(50, -30, 25);
 
+/*
 fs.writeFileSync(__filename + '.dxf', d.toDxfString());
+*/
 
-export default function handler(req, res) {
-  res.setHeader('X-Version', '666');
+  res.setHeader('X-Version', version);
+  res.status(200).send(d.toDxfString());
+/*
   res.status(200).json({
     "req.method": req.method,
     "req.query": req.query,
@@ -27,4 +46,5 @@ export default function handler(req, res) {
     "req.body": req.body,
     "req.cookies": req.cookies,
   })
+*/
 }
